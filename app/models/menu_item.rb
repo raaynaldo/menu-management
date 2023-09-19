4c7aa46562ed70d453e7aa86d250e07b992cbe27
Raynaldo Sutisna <raaynaldo@gmail.com>
2023-09-19 13:07:18 -0400

    feat: MenuItem names should not be duplicated in the database
    Update menu_items_spec.
    Update menu_item model validation.

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,6 +1,6 @@
 class MenuItem < ApplicationRecord
   belongs_to :menu
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :price, presence: true, numericality: { greater_than: 0 }
 end
--- spec/models/menu_item_spec.rb
+++ spec/models/menu_item_spec.rb
@@ -30,5 +30,17 @@ RSpec.describe MenuItem, type: :model do
     it "is valid if price is present" do
       expect(subject).to be_valid
     end
+
+    it "is invalid if name is already exist" do
+      subject.save()
+      menu_item = described_class.new(name: "Burger", price: 5, menu_id: subject.menu_id)
+      expect(menu_item).to_not be_valid
+    end
+
+    it "is valid if name is not already exist" do
+      subject.save()
+      menu_item = described_class.new(name: "French Fries", price: 5, menu_id: subject.menu_id)
+      expect(menu_item).to be_valid
+    end
   end
 end
