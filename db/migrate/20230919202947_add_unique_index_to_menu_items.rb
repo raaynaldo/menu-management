147013e440bb711c52590c7cc33df024c7454b15
Raynaldo Sutisna <raaynaldo@gmail.com>
2023-09-20 20:00:09 -0400

    fix: migration script issue

--- db/migrate/20230919202947_add_unique_index_to_menu_items.rb
+++ db/migrate/20230919202947_add_unique_index_to_menu_items.rb
@@ -1,5 +1,5 @@
 class AddUniqueIndexToMenuItems < ActiveRecord::Migration[7.0]
   def change
-    add_index :menu_items, [:name, :restaurant], unique: true
+    add_index :menu_items, [:name, :restaurant_id, :price], unique: true
   end
 end
--- db/migrate/20230920161609_update_unique_index_to_menu_items.rb
+++
@@ -1,6 +0,0 @@
-class UpdateUniqueIndexToMenuItems < ActiveRecord::Migration[7.0]
-  def change
-    remove_index :menu_items, name: "index_menu_items_on_name_and_restaurant" if index_exists?(:menu_items, :name, name: "index_menu_items_on_name_and_restaurant")
-    add_index :menu_items, [:name, :restaurant_id, :price], unique: true
-  end
-end
